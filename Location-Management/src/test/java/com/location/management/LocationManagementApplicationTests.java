769723bcd44de06f48a33eb3fbdc8050f2c92e65
Joshua <xulujoshua@gmail.com>
2022-07-20 10:58:59 +0200

    Initial commit

---
+++ Location-Management/src/test/java/com/location/management/LocationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.location.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
